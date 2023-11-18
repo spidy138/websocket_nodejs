f43e25360cda7c8a2266ffeb82e606ccf2eca6bf
achyuth <pachyuth@dovercorp.com>
2023-11-18 14:49:09 +0530

    added the basic socket code  with braodcast and emit

--- websocket_nodejs/project/src/app.js
+++ websocket_nodejs/project/src/app.js
@@ -4,12 +4,14 @@ const swaggerJsdoc=require('swagger-jsdoc');
 const swaggerUI=require('swagger-ui-express');
 const customerRoute=require('./routes/customerRoute.js');
 const config=require('./config/configuration.js');
-
-
+const socketIO=require('socket.io');
+const http=require('http');
+const cors=require('cors');
 var port=3000;
 var socketPort=4000;
 var app=express();
 app.use(bodyParser.json());
+app.use(cors);
 // const swaggerdefinition=require('../src/config/swaggerDef');
 const definition ={
   info: {
@@ -43,7 +45,35 @@ app.get('/about',(req,res)=>{
 res.send("<h1>Hey, it works!</h1>");
 });
 app.use('/',customerRoute);
-app.listen(port,()=>{
-  console.log("server started listening on "+port)
+
+
+
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
 });
 
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+  socket.on('message',function(data){
+    socket.emit('message', data);
+    io.sockets.emit('broadcast',{ description: data + ' data to  all the client from connected!'});  
+  });
+  
+})
+
+
+server.listen(3000);
